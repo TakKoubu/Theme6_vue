e931263ba3c3d26f1fe2ac01e0fedab6854bb2e4
TakKokubu <kokubu.tk@gmail.com>
2021-12-28 22:09:24 +0900

    課題6

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>
+    <strong>Name:</strong>
+    {{ name }}
+  </h2>
+  <h3>
+    <strong>Age:</strong>
+    {{ age }}
+  </h3>
+</template>
+
+<script>
+export default {
+  props: ['id', 'name' , 'age'],
+  data() {
+    return {
+      // detailsAreVisible: false
+    };
+  },
+  methods: {
+    // toggleDetails() {
+    //   this.detailsAreVisible = !this.detailsAreVisible;
+    // },
+    // toggleFavorite() {
+    //   this.$emit('toggle-favorite', this.id);
+    // }
+  },
+};
+</script>
---
+++ src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>age</label>
+      <input type="text" v-model="enteredAge" />
+    </div>
+    <div>
+      <button>Add</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-profile'],
+  data() {
+    return {
+      enteredName: '',
+      enteredAge: '',
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        'add-profile',
+        this.enteredName,
+        this.enteredAge,
+      );
+    },
+  },
+};
+</script>
